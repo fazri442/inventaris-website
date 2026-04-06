2b049102b2e2c0186b84b82f4756ccf802d919e6
fazri442 <zriganz442@email.com>
2026-04-06 08:12:23 +0700

    push baru

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    
+    Route::resource('/tim', \App\Http\Controllers\Api\TimApiController::class)->except(['create', 'edit']);
+    Route::resource('/barangkeluar', \App\Http\Controllers\Api\BarangKeluarApiController::class)->except(['create', 'edit']);
+    Route::resource('/datapusat', \App\Http\Controllers\Api\DataPusatApiController::class)->except(['create', 'edit']);
+    Route::resource('/barangmasuk', \App\Http\Controllers\Api\BarangMasukApiController::class)->except(['create', 'edit']);
+    Route::resource('/barangkeluar', \App\Http\Controllers\Api\BarangKeluarApiController::class)->except(['create', 'edit']);
+    Route::resource('/peminjaman', \App\Http\Controllers\Api\PeminjamApiController::class)->except(['create', 'edit']);
+    Route::resource('/pengembalian', \App\Http\Controllers\Api\PengembalianApiController::class)->except(['create', 'edit']);
 });
